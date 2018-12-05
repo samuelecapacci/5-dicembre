1ad87011f8662878d3aa3d2558b0ed8945243970
samuele <samuele.capacci@libero.it>
2018-12-05 13:03:15 +0100

    fanculo

--- src/components/post/post.js
+++ src/components/post/post.js
@@ -3,13 +3,26 @@ import React, { Component } from 'react';
 class Post extends Component {
     constructor(props) {
         super(props);
+        this.State({
+            post: []
+        })
     }
-    render() {
-        return (
-            <div className="post">
-                dio
+    dataFetch() {
+        fetch('https://jsonplaceholder.typicode.com/posts')
+            .then(response => response.json())
+            .then(json => this.setState({
+                post: json
+            }
+            ))
+}
+
+render() {
+    return (
+        <div className="post">
+            dio
             </div>
-        );
-    }
+    );
+}
+
 }
 export default Post;
