274c5650de8f7be005757501722086a4144073ea
samuele <samuele.capacci@libero.it>
2018-12-05 10:17:30 +0100

    dio po

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import navbar from './components/navbar/Navbar';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-      
+      <navbar />
       </div>
     );
   }
