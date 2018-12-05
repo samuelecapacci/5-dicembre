5b5c690c1f6995006ab9d7219f21abaa464a8290
samuele <samuele.capacci@libero.it>
2018-12-05 13:41:02 +0100

    ultimo commit

--- src/components/post/post.js
+++ src/components/post/post.js
@@ -3,26 +3,36 @@ import React, { Component } from 'react';
 class Post extends Component {
     constructor(props) {
         super(props);
-        this.State({
+        this.state = {
             post: []
-        })
+        }
     }
+    
     dataFetch() {
         fetch('https://jsonplaceholder.typicode.com/posts')
-            .then(response => response.json())
-            .then(json => this.setState({
+        .then((response) => response.json())
+        .then((json) => this.setState({
                 post: json
-            }
-            ))
-}
+            })
+        )
+    }
 
-render() {
-    return (
-        <div className="post">
-            dio
+    render() {
+        return (
+            <div className="post">
+                {this.state.post.map((item) => 
+                    <div>
+                    <div>{item.id}</div>
+                    <div>{item.userId}</div>
+                    <div>{item.title}</div>
+                    <div>{item.body}</div>
+                </div>
+                )
+
+                }
             </div>
-    );
-}
+        );
+    }
 
 }
 export default Post;
